78c0bc3bf12f282926f920b2159d8385d0a53d51
dangoldmann <101563504+dangoldmann@users.noreply.github.com>
2022-09-23 12:00:27 -0300

    feat: Created jwt example

---
+++ cookieJwtAuth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+const cookieJwtAuth = (req, res, next) => {
+    const token = req.cookies.token
+    
+    try {
+        const user = jwt.verify(token, process.env.SECRET_KEY)
+        req.user = user
+        next()
+    }
+    catch (err) {
+        res.clearCookie('token')
+        return res.send('Fail')
+    }
+}
+
+module.exports = {cookieJwtAuth}
--- index.js
+++ index.js
@@ -2,25 +2,53 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 const apiErrorHandler = require('./error/api-error-handler');
 const ApiError = require('./error/ApiError');
 const {router: userRoutes, basePath: userBasePath} = require('./routes/users')
 const {router: radiographyRoutes, basePath: radiographyBasePath} = require('./routes/radiographies')
+const {cookieJwtAuth} = require('./cookieJwtAuth')
 
 // middleware
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 //routes
 app.use(userBasePath, userRoutes)
 app.use(radiographyBasePath, radiographyRoutes)
 
+app.get('', (req, res) => {
+    const user = {
+        id: 1,
+        name: 'name'
+    }
+
+    const token = jwt.sign({user}, process.env.SECRET_KEY, {expiresIn: '24h'})
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 86400
+    })
+
+    res.redirect('/home')
+
+})
+
+app.get('/home', cookieJwtAuth, (req, res) => {
+    
+    res.send('You are in the Home Page')
+})
+
 app.use((req, res, next) => {
+    console.log(req.path)
     const error = new ApiError(404, 'Not found')
     next(error)
 })
 
 app.use(apiErrorHandler)
+
 
 // starting the server
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`))
