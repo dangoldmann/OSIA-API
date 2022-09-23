cc594f4d84f0127dbdde6776c03e440ae70554d3
dangoldmann <101563504+dangoldmann@users.noreply.github.com>
2022-09-23 15:02:46 -0300

    feat: Created test file to test jwt

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const jwt = require('jsonwebtoken')
 const apiErrorHandler = require('./error/api-error-handler');
 const ApiError = require('./error/ApiError');
 const {router: userRoutes, basePath: userBasePath} = require('./routes/users')
 const {router: radiographyRoutes, basePath: radiographyBasePath} = require('./routes/radiographies')
-const {cookieJwtAuth} = require('./cookieJwtAuth')
+const {router: testRoutes, basePath: testBasePath} = require('./test')
 
 // middleware
 app.use(express.json())
@@ -18,28 +17,7 @@ app.use(cookieParser())
 //routes
 app.use(userBasePath, userRoutes)
 app.use(radiographyBasePath, radiographyRoutes)
-
-app.get('', (req, res) => {
-    const user = {
-        id: 1,
-        name: 'name'
-    }
-
-    const token = jwt.sign({user}, process.env.SECRET_KEY, {expiresIn: '24h'})
-
-    res.cookie('token', token, {
-        httpOnly: true,
-        maxAge: 86400
-    })
-
-    res.redirect('/home')
-
-})
-
-app.get('/home', cookieJwtAuth, (req, res) => {
-    
-    res.send('You are in the Home Page')
-})
+app.use(testBasePath, testRoutes)
 
 app.use((req, res, next) => {
     console.log(req.path)
@@ -48,7 +26,6 @@ app.use((req, res, next) => {
 })
 
 app.use(apiErrorHandler)
-
 
 // starting the server
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`))
