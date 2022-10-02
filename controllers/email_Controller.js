466e46be5f8a72004739ee75583669f24325d702
dangoldmann <goldmandan8@gmail.com>
2022-10-01 23:27:32 -0300

    feat: Created email controller

---
+++ controllers/email_Controller.js
@@ -0,0 +1,23 @@
+const client = require('@sendgrid/mail')
+
+client.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendResetPasswordEmail = (email, link) => {
+    client
+        .send({
+            to: email,
+            from: 'osiaapp@gmail.com',
+            templateId: 'd-552e2fec003a49e8a2e42f3fb788a9ba',
+            dynamicTemplateData: {
+                link
+            }
+        })
+        .then(() => {
+            console.log('Email sent')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+module.exports = {sendResetPasswordEmail}
