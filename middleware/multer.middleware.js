dfd80fc95d9aa30994459d736bc54c7db4cd5136
dangoldmann <goldmandan8@gmail.com>
2022-10-09 21:42:51 -0300

    feat: File filter added

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -5,13 +5,18 @@ const storage = multer.diskStorage({
     destination: path.join(__dirname, '../public/images'),
     filename: (req, file, cb) => {
         cb(null, file.originalname)
+        //cb(null, `${file.fieldname} - ${Date.now().To} ${path.extname(file.originalname)}`)
     }
 })
 
 const upload = multer({
     storage,
     dest: 'public/images',
-    limits: {fileSize: 5000000}
+    limits: {fileSize: 5000000},
+    fileFilter: (req, file, cb) => {
+        if(file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') return cb(null, true)
+        return cb(new Error('El archivo debe ser .png, .jgp o .jpeg'))
+    }
 }).single('image')
 
 
