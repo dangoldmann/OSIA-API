51ec4f70ad15d788daf86989bc5bc8d28499fcee
dangoldmann <goldmandan8@gmail.com>
2022-10-09 21:43:22 -0300

    feat: Error handling added to image uploading

--- test/uploadImages.js
+++ test/uploadImages.js
@@ -1,16 +1,20 @@
 const router = require('express').Router()
-
+const ApiError = require('../error/ApiError')
 const {upload} = require('../middleware/multer.middleware')
 
 const basePath = '/images'
 
-router.post('/upload', upload, (req, res) => {
-    const image = req.file
-    console.log(image)
+router.post('/upload', (req, res, next) => {
+    upload(req, res, err => {
+        if(err) return next(ApiError.badRequest(err.message))
 
-    const message = image ? 'Image uploaded successfully' : 'There was a problem'
+        const image = req.file
 
-    res.send({message})
+        const message = image ? 'Image uploaded successfully' : 'There was a problem'
+
+        res.send({message})
+    })
+    
 })
 
 module.exports = {router, basePath}
