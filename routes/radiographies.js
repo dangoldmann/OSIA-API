27490f6353a8697b5ba831b380178a229a1fab4b
dangoldmann <goldmandan8@gmail.com>
2022-10-10 16:54:35 -0300

    feat: /upload route created

--- routes/radiographies.js
+++ routes/radiographies.js
@@ -1,25 +1,26 @@
 const router = require('express').Router()
 const radiographyController = require('../controllers/radiography_Controller')
+const jwt = require('jsonwebtoken')
 const ApiError = require('../classes/ApiError')
+const {upload} = require('../middleware/multer.middleware')
 
 const basePath = '/radiographies'
 
-router.post('/add', async (req, res, next) => {
-    const {imageRoute, bodyPart, userId} = req.body
-    
-    if(!imageRoute || !bodyPart || !userId){
-        next(ApiError.badRequest('You must complete all the fields'))
-        return
-    }
+router.post('/upload', async (req, res, next) => {
+    upload(req, res, err => {
+        if(err) next(ApiError.badRequest(err.message))
+    })
 
-    const radiographyInfo = {imageRoute, bodyPart, userId}
-    
+    const access_token = req.cookies.access_token
+    const user = jwt.verify(access_token, process.env.SECRET_KEY).user
+
+    const imageRoute = `./public/images/${user.id}`
+
+    const radiographyInfo = {imageRoute, bodyPart: 'Brazo', userId: user.id}
+
     const radiography = await radiographyController.create(radiographyInfo, next)
 
-    if(radiography){
-        res.status(201).send({radiography})
-    }
-    
+    if(radiography) res.status(201).send({radiography})
 })
 
 router.get('', async (req, res, next) => {
