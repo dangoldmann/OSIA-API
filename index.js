cb41fc8242cb78d8e38a341ecce995b167c04cf5
dangoldmann <goldmandan8@gmail.com>
2022-10-09 22:50:28 -0300

    Changed index file

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const {router: radiographyRoutes, basePath: radiographyBasePath} = require('./ro
 const {router: cookiesTestRoutes, basePath: cookiesTestBasePath} = require('./test/cookiesTest');
 const {router: imagesTestRoutes, basePath: imagesTestBasePath} = require('./test/uploadImages');
 
-// SET UP
+// SETTINGS
+app.set('view engine', 'ejs')
+
+// MIDDLEWARES
 app.use(express.json())
 app.use(cors({
     origin: 'http://127.0.0.1:5500',
@@ -19,7 +22,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: false}))
-app.set('view engine', 'ejs')
+
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -31,14 +34,14 @@ app.use(userBasePath, userRoutes)
 app.use(radiographyBasePath, radiographyRoutes)
 app.use(cookiesTestBasePath, cookiesTestRoutes)
 app.use(imagesTestBasePath, imagesTestRoutes)
-
 app.use((req, res, next) => {
     console.log(req.path)
     const error = new ApiError(404, 'Not found')
     next(error)
 })
 
+// ERROR HANDLING
 app.use(apiErrorHandler)
 
-// starting the server
+// STARTING THE SERVER
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`))
