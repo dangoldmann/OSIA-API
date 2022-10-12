9fc9c8619863d897ae2f684ca1f597b2788bf4ec
dangoldmann <goldmandan8@gmail.com>
2022-10-11 22:56:48 -0300

    Simplified responses

--- routes/users.js
+++ routes/users.js
@@ -25,10 +25,7 @@ router.post('/register', signUpSchema, validator, async (req, res, next) => {
 
         res.cookie('access_token', access_token, cookieOptions)
         .status(201)
-        .send({
-            user,
-            access_token
-        })
+        .send()
     }
 })
 
