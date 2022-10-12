db2f0bdbe93f26178c9726ed4d2f05971aae67bc
dangoldmann <goldmandan8@gmail.com>
2022-10-11 23:48:16 -0300

    feat: Created refresh-token route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const auth_Controller = require('../controllers/auth_Controller')
-const {signAccessToken} = require('../helpers/jwtHelper')
+const {signAccessToken, signRefreshToken, verifyRefreshToken} = require('../helpers/jwtHelper')
 const {isLoggedIn} = require('../middleware/cookies.middleware')
 const {signUpSchema, logInSchema} = require('../helpers/validators')
 const {validator} = require('../middleware/validator.middleware')
 const {cookieOptions} = require('../config')
+const createError = require('http-errors')
 
 const basePath = '/auth'
 
@@ -46,14 +47,43 @@ router.post('/login', logInSchema, validator, async (req, res, next) => {
 
     if(user) {
         const access_token = signAccessToken(user.id)
+        const refresh_token = signRefreshToken(user.id)
 
         res.cookie('access_token', access_token, cookieOptions)
-        .send({})
+        .send({access_token, refresh_token})
     }
 })
 
 router.post('/refresh-token', (req, res, next) => {
-    
+    try {
+        const {refreshToken} = req.body    
+        if(!refreshToken) return next(createError.BadRequest('Token not found'))
+
+        const payload = verifyRefreshToken(refreshToken)
+
+        if(payload.message) {
+            res.clearCookie('refresh_token', {sameSite: 'none', secure: true})
+        
+            return res.status(401).send({
+                error: {
+                    status: 401,
+                    message: payload.message
+                },
+                redirect: {
+                    destination: './LogIn.html'
+                }
+            })
+        }
+
+        const userId = payload.id
+
+        const access_token = signAccessToken(userId)
+        const refresh_token = signRefreshToken(userId)
+
+        res.send({access_token, refresh_token})
+    } catch (error) {
+        return next(error)
+    }
 })
 
 module.exports = {router, basePath}
