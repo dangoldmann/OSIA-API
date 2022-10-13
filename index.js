8e9d09b778c9e02b25e871754f6482cf304fe7ef
dangoldmann <101563504+dangoldmann@users.noreply.github.com>
2022-10-13 10:24:09 -0300

    fix: localhost domain added as origin for cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 // MIDDLEWARES
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
     credentials: true
 }));
 app.use(cookieParser());
