1a50af849e7505f5ce5477561df4b92229da3c99
dangoldmann <101563504+dangoldmann@users.noreply.github.com>
2022-10-14 11:26:50 -0300

    feat: Updated validators

--- helpers/validators.js
+++ helpers/validators.js
@@ -1,16 +1,16 @@
 const {body} = require('express-validator')
 
 const signUpSchema = [
-    body('name', 'Ingrese un nombre completo').isLength({min: 3}),
-    body('surname', 'Ingrese un apellido completo').isLength({min: 4}),
+    body('name', 'El nombre debe contener entre 3 y 15 caracteres').isLength({min: 3, max: 15}),
+    body('surname', 'El apellido debe contener entre 4 y 25 caracteres').isLength({min: 4, max: 25}),
     body('email', 'Ingrese un email valido').isEmail(),
     body('phone', 'Ingres un número de telefono válido').isMobilePhone(),
-    //body('password', 'La contraseña debe de ser como mínimo de 6 caracteres').isLength({min: 6})
+    //body('password', 'La contraseña debe contener entre 6 y 30 caracteres').isLength({min: 6, max: 30})
 ]
 
 const logInSchema = [
     body('email', 'Ingrese un email valido').isEmail(),
-    //body('password', 'La contraseña debe de ser como mínimo de 6 caracteres').isLength({min: 6})
+    //body('password', 'La contraseña debe contener entre 6 y 30 caracteres').isLength({min: 6, max: 30})
 ]
 
 module.exports = {signUpSchema, logInSchema}
