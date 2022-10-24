0c21691e63efe7736541ec5b7558bac8baa384a1
dangoldmann <101563504+dangoldmann@users.noreply.github.com>
2022-10-24 15:38:07 -0300

    feat: Deleted image test route

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const createError = require('http-errors');
-const {cookieJwtAuth} = require('./middleware/cookies.middleware');
 const errorHandler = require('./middleware/error.middleware');
 const {router: authRoutes, basePath: authBasePath} = require('./routes/auth');
 const {router: radiographyRoutes, basePath: radiographyBasePath} = require('./routes/radiographies');
 const {router: userRoutes, basePath: userBasePath} = require('./routes/users');
-const fs = require('fs')
 
 // SETTINGS
 app.use(express.static(__dirname + '/views/public'))
@@ -26,13 +24,6 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // ROUTES
-app.get('/images', (req, res) => {
-    fs.readdir('./public/images/1/', (err, files) => {
-        files.forEach(file => console.log(file))
-    })
-})
-app.get('/', cookieJwtAuth, () => {});  
-
 app.use(authBasePath, authRoutes);
 app.use(radiographyBasePath, radiographyRoutes);
 app.use(userBasePath, userRoutes);
