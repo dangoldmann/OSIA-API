4b2864e5567d046aa8fb4ec059928065bea8232a
dangoldmann <goldmandan8@gmail.com>
2022-10-28 16:14:33 -0300

    Delete originValidator.js

--- originValidator.js
+++
@@ -1,8 +0,0 @@
-const originValidator = (req, res, next) => {
-    if(req.get('origin') === 'http://127.0.0.1:5500'){
-        return next()
-    }
-    res.sendStatus(403)
-}
-
-module.exports = {originValidator}
