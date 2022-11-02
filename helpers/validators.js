f43c5df354445cd288d41d48821841ac1f1a25b7
dangoldmann <goldmandan8@gmail.com>
2022-11-02 18:33:50 -0300

    fixed validation schemas

--- helpers/validators.js
+++ helpers/validators.js
@@ -5,12 +5,11 @@ const signUpSchema = [
     body('surname', 'El apellido debe contener entre 4 y 25 caracteres').isLength({min: 4, max: 25}),
     body('email', 'Ingrese un email valido').isEmail(),
     body('phone', 'Ingres un número de telefono válido').isMobilePhone(),
-    //body('password', 'La contraseña debe contener entre 6 y 30 caracteres').isLength({min: 6, max: 30})
+    body('password', 'La contraseña debe contener entre 6 y 30 caracteres').isLength({min: 6, max: 30})
 ]
 
 const logInSchema = [
-    body('email', 'Ingrese un email valido').isEmail(),
-    //body('password', 'La contraseña debe contener entre 6 y 30 caracteres').isLength({min: 6, max: 30})
+    body('email', 'Ingrese un email valido').isEmail()
 ]
 
 module.exports = {signUpSchema, logInSchema}
