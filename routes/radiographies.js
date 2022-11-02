d90710d9bf525e0fd74f372337e92021503fbfe6
dangoldmann <goldmandan8@gmail.com>
2022-11-02 18:41:19 -0300

    test

--- routes/radiographies.js
+++ routes/radiographies.js
@@ -27,11 +27,11 @@ router.post('/upload', verifyToken, (req, res, next) => {
 
             const radiography = await radiographyController.create(radiographyInfo, next)
 
-            const aiPrediction = await predictAI(radiography.id.toString())
-            const buffer = Buffer.from(aiPrediction, 'base64')
+            // const aiPrediction = await predictAI(radiography.id.toString())
+            // const buffer = Buffer.from(aiPrediction, 'base64')
 
-            const dirname = __dirname.substring(0, __dirname.length - 7)
-            fs.writeFileSync(dirname + path.join(imageRoute), buffer)
+            // const dirname = __dirname.substring(0, __dirname.length - 7)
+            // fs.writeFileSync(dirname + path.join(imageRoute), buffer)
 
             if(radiography) res.send({
                 radiographyId: radiography.id
