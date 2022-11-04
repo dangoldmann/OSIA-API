c686e97b880c54bcbe91ebe820c3b1a77252def0
dangoldmann <goldmandan8@gmail.com>
2022-11-04 01:12:22 -0300

    feat: now the body of the requests sends the imagePath instead of the id

--- helpers/ai.js
+++ helpers/ai.js
@@ -1,20 +1,21 @@
 const fetch = require('node-fetch')
 const {AIUrl} = require('../config')
 
-const predictAI = async radiographyId => {
-    try {let res = await fetch(AIUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify({
-            id: radiographyId
+const predictAI = async imagePath => {
+    try {
+        let res = await fetch(AIUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                imagePath
+            })
         })
-    })
 
-    res = await res.json()
+        res = await res.json()
 
-    return res.image_base64
+        return res.image_base64
     } catch (error) {
         return {error: {message: error.message}}
     }
