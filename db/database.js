02a8047f3fa3e0327b1f29de58f2fd665bf15e13
ataefeivan@gmail.com <88833116+IvanAtaefe@users.noreply.github.com>
2023-09-18 22:35:19 -0300

    yarn added

--- db/database.js
+++ db/database.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
 const mysql = require('mysql2')
 
-const connection = mysql.createConnection(process.env.DB_URL) // PLANET SCALE CONNECTION
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    database: 'mysql',
+    password: 'rootroot'
+    }); // PLANET SCALE CONNECTION
 
 module.exports = connection.promise()
